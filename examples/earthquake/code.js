64cc3543e0c2eafc6da6874a94616a6e1da89948
behicsakar <behicsakar@gmail.com>
2016-06-20 19:33:28 +0300

    earthquake before 'making it efficient'

--- examples/earthquake/code.js
+++ examples/earthquake/code.js
@@ -6,22 +6,95 @@
  * We make no guarantees that this code is fit for any purpose.
  * Visit http://www.pragmaticprogrammer.com/titles/smreactjs for more book information.
 ***/
-var quakes = Rx.Observable
-  .interval(5000)
-  .flatMap(function() {
-   return Rx.DOM.jsonpRequest({
-      url: QUAKE_URL,
-      jsonpCallback: 'eqfeed_callback'
-    }).retry(3)
-  })
-  .flatMap(function(result) {
-    return Rx.Observable.from(result.response.features);
-  })
-  .distinct(function(quake){
-    return quake.properties.code
-  })
-quakes.subscribe(function(quake) {
-  var coords = quake.geometry.coordinates
-  var size = quake.properties.mag * 10000
-  L.circle([coords[1], coords[0]], size).addTo(map);
-});
+
+var codeLayers = {};
+var quakeLayer = L.layerGroup([]).addTo(map);
+var identity = Rx.helpers.identity; //(1)
+
+function isHovering(element) {
+  var over = Rx.DOM.mouseover(element).map(identity(true)); //(2)
+  var out = Rx.DOM.mouseout(element).map(identity(false)); //(3)
+
+  return over.merge(out); //(4)
+
+}
+
+function makeRow(props) {
+  var row = document.createElement('tr');
+  row.id = props.net + props.code;
+
+  var date = new Date(props.time);
+  var time = date.toString();
+  [props.place, props.mag, time].forEach(function(text) {
+    var cell = document.createElement('td');
+    cell.textContent = text;
+    row.appendChild(cell);
+  });
+
+  return row;
+}
+
+function initialize() {
+  var quakes = Rx.Observable
+    .interval(5000)
+    .flatMap(function() {
+     return Rx.DOM.jsonpRequest({
+        url: QUAKE_URL,
+        jsonpCallback: 'eqfeed_callback'
+      }).retry(3)
+    })
+    .flatMap(function(result) {
+      return Rx.Observable.from(result.response.features);
+    })
+    .distinct(function(quake){
+      return quake.properties.code
+    }).share()
+
+  quakes.subscribe(function(quake) {
+    var coords = quake.geometry.coordinates
+    var size = quake.properties.mag * 10000
+
+    var circle = L.circle([coords[1], coords[0]], size).addTo(map);
+    quakeLayer.addLayer(circle);
+    codeLayers[quake.id] = quakeLayer.getLayerId(circle);
+  });
+
+  var table = document.getElementById('quakes_info');
+
+  quakes
+    .pluck('properties')
+    .map(makeRow)
+    .bufferWithTime(500)
+    .filter(function(rows){ return rows.length > 0; })
+    .map(function(rows) {
+      var fragment = document.createDocumentFragment();
+      rows.forEach(function(row){
+        fragment.appendChild(row);
+      });
+      return fragment
+    })
+    .subscribe(function(fragment) {
+      var row = fragment.firstChild;
+      var circle = quakeLayer.getLayer(codeLayers[row.id]);
+
+      isHovering(row)
+        .subscribe(function(hovering) {
+          circle.setStyle({
+            color: hovering ?
+              '#ff0000' :
+              '#0000ff'
+          });
+        });
+
+      Rx.DOM
+        .click(row)
+        .subscribe(function() {
+          map.panTo(circle.getLatLng());
+          console.log(circle.getLatLng());
+        });
+
+      table.appendChild(fragment);
+    });
+}
+
+Rx.DOM.ready().subscribe(initialize);
